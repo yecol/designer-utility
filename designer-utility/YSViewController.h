a84ffa3b94bf13908bd3ba9e05abee7b6f1916ed
yecol <xujb1987@gmail.com>
2014-03-01 00:33:45 +0800

    index page finished.

--- designer-utility/YSViewController.h
+++ designer-utility/YSViewController.h
@@ -8,6 +8,20 @@
 
 #import <UIKit/UIKit.h>
 
-@interface YSViewController : UIViewController
+@interface YSViewController : UIViewController<UIScrollViewDelegate>
+
+@property(assign,nonatomic)IBOutlet UIButton *portfolioBtn;
+@property(assign,nonatomic)IBOutlet UIView *portfolioIndexView;
+@property(assign,nonatomic)IBOutlet UIImageView *avatarView;
+
+@property(assign,nonatomic)IBOutlet UIScrollView *hPflView;
+@property(assign,nonatomic)IBOutlet UIScrollView *v1PflView;
+@property(assign,nonatomic)IBOutlet UIScrollView *v2PflView;
+@property(assign,nonatomic)IBOutlet UIScrollView *v3PflView;
+@property(assign,nonatomic)IBOutlet UIScrollView *v4PflView;
+
+
+
+-(IBAction)portfolioBtnPressed:(id)sender;
 
 @end
--- designer-utility/YSViewController.m
+++ designer-utility/YSViewController.m
@@ -8,7 +8,11 @@
 
 #import "YSViewController.h"
 
-@interface YSViewController ()
+@interface YSViewController (){
+    
+    BOOL isPortfolioOpen;
+    CGRect oViewFrame, oBtnFrame;
+}
 
 @end
 
@@ -18,12 +22,85 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    isPortfolioOpen = FALSE;
+    
+    [self.view bringSubviewToFront:self.portfolioBtn];
+    
+    [self.hPflView setScrollEnabled:YES];
+    [self.hPflView setContentSize:CGSizeMake(2000, 700)];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(IBAction)portfolioBtnPressed:(id)sender{
+    
+    //portfolio index view animation
+    
+    float bannerHeight = 68;
+    
+    if (!isPortfolioOpen) {
+        
+        oViewFrame = self.portfolioIndexView.frame;
+        oBtnFrame = self.portfolioBtn.frame;
+        
+        NSLog(@"oBtnFramex=%f,y=%f,w=%f,h=%f",oBtnFrame.origin.x,oBtnFrame.origin.y,oBtnFrame.size.width,oBtnFrame.size.height);
+        
+        [UIView animateWithDuration:0.5
+                              delay:0.1
+                            options: UIViewAnimationOptionCurveEaseInOut
+                         animations:^
+         {
+             CGRect viewFrame = oViewFrame;
+             viewFrame.origin.y = bannerHeight;
+             self.portfolioIndexView.frame = viewFrame;
+             
+             CGRect avatarFrame = oBtnFrame;
+             avatarFrame.origin.y = -20;
+             self.portfolioBtn.frame = avatarFrame;
+             
+             [self.portfolioBtn layer].transform = CATransform3DMakeScale(0.5,0.5,1);
+         }
+                         completion:^(BOOL finished)
+         {
+             [self.portfolioBtn setImage:[UIImage imageNamed:@"avatar_down"] forState:UIControlStateHighlighted];
+             isPortfolioOpen = TRUE;
+         }];
+        
+    }else{
+        [UIView animateWithDuration:0.5
+                              delay:0.1
+                            options: UIViewAnimationOptionCurveEaseInOut
+                         animations:^
+         {
+             self.portfolioIndexView.frame = oViewFrame;
+             
+             [self.portfolioBtn layer].transform = CATransform3DMakeScale(1,1,1);
+             self.portfolioBtn.frame = oBtnFrame;
+         }
+                         completion:^(BOOL finished)
+         {
+             [self.portfolioBtn setImage:[UIImage imageNamed:@"avatar_up"] forState:UIControlStateHighlighted];
+             isPortfolioOpen = FALSE;
+         }];
+    }
+    
+
+}
+
+-(void)beginIndexPageAnimation{
+    NSLog(@"begin Index Animation");
+}
+
+-(void)scrollViewDidScrollToTop:(UIScrollView *)scrollView{
+    NSLog(@"1");
+}
+-(void)scrollViewDidScroll:(UIScrollView *)scrollView{
+    NSLog(@"111");
 }
 
 @end
